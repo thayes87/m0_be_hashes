004d1ce819470d46a87566468b3de7426df6391a
Thomas Hayes <hayes.thomas610@gmail.com>
2022-06-09 13:35:07 -0600

    Initial commit

--- syntax_cleanup.rb
+++ syntax_cleanup.rb
@@ -1,23 +1,53 @@
 # Clean Up the code below to improve readability.
-foods = {"apples": 23, 
-  "grapes": 507, 
+foods = {"apples": 23,
+  "grapes": 507,
   "eggs": 48}
+
+  foods = {
+    "apples" => 23,
+    "grapes" => 507,
+    "eggs" => 48,
+  }
+
+puts foods.keys
+puts foods.values
 
 person = { "height": "6 ft", "weight": "160 lbs" }
 
-post = { image_src: "./images/beach.png",
-
-
-   caption: "At the beach with my besties", timestamp: "4:37 PM August 13, 2019",
-        number_likes: 0,
-comments: [] }
-
-
-post2 =
-{
-      image_src: "./images/holiday-party.png",
-      caption: "What a great holiday party omg",
-      timestamp: "11:37 PM December 31, 2019",
-      number_likes: 13,
-      comments: []
+person = {
+  "height" => "6 ft",
+  "weight" => "weight",
 }
+
+puts person.keys
+puts person.values
+
+# post = { image_src: "./images/beach.png",
+#
+#
+#    caption: "At the beach with my besties", timestamp: "4:37 PM August 13, 2019",
+#         number_likes: 0,
+# comments: [] }
+
+post = {
+  "image search" => "./images/beach.png",
+  "caption" => "At the beach with my besties",
+  "time stamp" => "4:37 PM August 13, 2019",
+  "number_likes" => 0,
+  "comments" => "hey",
+}
+
+puts post["time stamp"]
+puts post.values
+
+
+
+post2 = {
+      "image_src:" => "./images/holiday-party.png",
+      "caption:" => "What a great holiday party omg",
+      "timestamp:" => "11:37 PM December 31, 2019",
+      "number_likes:" => "13",
+      "comments:" => "hey",
+}
+
+puts post2["caption:"]
